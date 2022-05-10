1801e1d342439adf2d05035adaa5bda4a08ec01c
Bonus <root@bonusplay.pl>
2022-05-10 22:59:53 +0200

    add some logging

--- app.py
+++ app.py
@@ -27,20 +27,22 @@ async def on_message(message):
         return
 
     if FLAG not in message.content:
+        logging.info("%s invalid flag (%s)", message.author.id, message.content)
         return await message.channel.send(f"It appears that the flag is not correct, try again?")
 
     guild = client.get_guild(GUILD_ID)
     role = guild.get_role(ROLE_ID)
     if not guild or not role:
+        logging.warning("guild: %s, role: %s", guild, role)
         return await message.channel.send(f"Something horrible has happened. Please contact <@{ADMIN_ID}>")
 
     member = await guild.fetch_member(message.author.id)
 
-    # in case someone left guild
-    # or something horrible has happened
     if not member:
+        logging.info("%s sent flag, but is not in the guild", message.author.id)
         return await message.channel.send(f"It appears that you're not a member of KNCyber server.")
 
+    logging.info("adding role to '%s'", message.author.id)
     await member.add_roles(role)
     await message.channel.send("Your flag seems to be correct! Welcome to KNCyber :)")
 
